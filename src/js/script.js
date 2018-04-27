f9ca9a3625fea3e516a20bfe1e8dbd0a409112c9
it-eria <iteria.info@gmail.com>
2018-04-27 20:53:48 +0300

    changes with rhombus

--- src/js/script.js
+++ src/js/script.js
@@ -16,13 +16,13 @@ $('.clients__list').slick({
     autoplaySpeed: 2000,
     infinity: true,
     dots: false,
-    arrows: false,
+    arrows: true,
     responsive: [
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 4,
-                slidesToScroll: 1,
+                slidesToShow: 3,
+                slidesToScroll: 3,
                 infinite: true,
                 dots: true
             }
@@ -30,8 +30,8 @@ $('.clients__list').slick({
         {
             breakpoint: 600,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1
+                slidesToShow: 2,
+                slidesToScroll: 2
             }
         },
         {
